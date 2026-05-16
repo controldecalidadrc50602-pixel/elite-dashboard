b2cdfe5ef20ac23d74fbdda3d654f4bc43189018
controldecalidadrc50602-pixel <controldecalidadrc50602@gmail.com>
2026-05-16 10:01:25 -0600

    🛠️ Solución Técnica Aplicada

--- src/components/Modals/ProjectSections/TechDNASection.tsx
+++ src/components/Modals/ProjectSections/TechDNASection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { Globe, Shield, Wifi } from 'lucide-react';
 import { TechDNA, Country, SipTrunkVirtual } from '../../../types/project';
 
